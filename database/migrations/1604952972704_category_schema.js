f178a40d9e15a71127f90a4b4550057d93d0a0ef
Gabriel <=>
2020-11-09 17:28:03 -0300

    product and image relationships

---
+++ database/migrations/1604952972704_category_schema.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CategorySchema extends Schema {
+  up () {
+    this.create('categories', (table) => {
+      table.increments()
+      table.string('title', 100)
+      table.string('description', 255)
+      table.integer('image_id').unsigned()
+      table.timestamps()
+
+      table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
+    })
+  }
+
+  down () {
+    this.drop('categories')
+    table.dropForeign('image_id')
+
+  }
+}
+
+module.exports = CategorySchema
