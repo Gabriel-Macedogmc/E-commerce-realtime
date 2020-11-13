9a56ce2e760a5426541b2b7f0ddd37becc38d19e
Gabriel <=>
2020-11-12 22:16:36 -0300

    Factory v1

--- database/factory.js
+++ database/factory.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Factory = require('@adonisjs/lucid/src/Factory')
+
 /*
 |--------------------------------------------------------------------------
 | Factory
@@ -14,8 +16,27 @@
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 // const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', (faker) => {
+  return {
+    username: faker.first(),
+    surname: faker.last(),
+    email: faker.email({ domain: 'fsocietybrasil.org' }),
+    password: 'secret'
+  }
+})
+
+Factory.blueprint('App/Models/Category', faker => {
+  return {
+    title: faker.country({ full: true }),
+    description: faker.sentence()
+  }
+})
+
+
+Factory.blueprint('App/Models/Product', faker => {
+  return {
+    name: faker.animal(),
+    description: faker.sentence(),
+    price: faker.floating({ min: 0, max: 1000, fixed: 2 })
+  }
+})
