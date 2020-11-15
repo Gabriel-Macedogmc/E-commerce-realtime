b57e1eab2c231873216ae46f9371c2c33e3ede3f
Gabriel <=>
2020-11-15 12:48:53 -0300

    Auth Controller

---
+++ app/Controllers/Http/Auth/AuthController.js
@@ -0,0 +1,32 @@
+'use strict'
+
+class AuthController {
+  async register({ request, response }) {
+    //
+  }
+  async login({ request, response, auth }) {
+    //
+  }
+
+  async refresh({ request, response }) {
+    //
+  }
+
+  async logout({ request, response, auth }) {
+    //
+  }
+
+  async forgot({ request, response }) {
+    //
+  }
+
+  async remeber({ request, response }) {
+    //
+  }
+
+  async reset({ request, response }) {
+    //
+  }
+}
+
+module.exports = AuthController
