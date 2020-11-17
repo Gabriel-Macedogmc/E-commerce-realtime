2ee33c303ce8e643f1b3f97c372b56e4de85b1ea
Gabriel <=>
2020-11-17 14:42:21 -0300

    Product Controller finalizado

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -7,6 +7,8 @@
 /**
  * Resourceful controller for interacting with products
  */
+
+const Product = use('App/Models/Product')
 class ProductController {
   /**
    * Show a list of all products.
@@ -17,7 +19,15 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, pagination }) {
+    const name = request.input('name')
+    const query = Product.query()
+    if (name) {
+      query.where('name', 'LIKE', `%${name}%`)
+    }
+    const products = await query.paginate(pagination.page, pagination.limit)
+
+    return response.status(200).send(products)
   }
 
   /**
@@ -28,7 +38,21 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    try {
+      const { name, description, price, image_id } = request.all()
+      const product = await Product.create({
+        name,
+        description,
+        price,
+        image_id,
+      })
+      return response.status(201).send(product)
+    } catch (error) {
+      return response
+        .status(404)
+        .send({ message: 'Erroa ao processar sua solicitação' })
+    }
   }
 
   /**
@@ -40,7 +64,9 @@ class ProductController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params: { id }, request, response, view }) {
+    const product = await Product.findOrFail(id)
+    return response.send(product)
   }
 
   /**
@@ -51,7 +77,18 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params: { id }, request, response }) {
+    try {
+      const product = await Product.findOrFail(id)
+      const { name, description, price, image_id } = request.all()
+      product.merge({ name, description, price, image_id })
+      await product.save()
+      return response.send(product)
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ message: 'Não foi possível atualizar este produto' })
+    }
   }
 
   /**
@@ -62,7 +99,14 @@ class ProductController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params: { id }, request, response }) {
+    try {
+      const product = await Product.findOrFail(id)
+      product.delete()
+      return response.status(204).send()
+    } catch (error) {
+      return response.status(500).send({ message: 'Não foi possível deleter este Produto' })
+    }
   }
 }
 
