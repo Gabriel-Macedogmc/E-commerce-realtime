76b79fd277a6205f507ff5668c221f68f2d077fe
Gabriel <=>
2020-11-19 20:53:51 -0300

    Admin Route add middlewares

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -14,13 +14,16 @@ Route.group(() => {
   Route.resource('coupons', 'CouponController').apiOnly()
 
   //Order resource routes
+  Route.post('orders/:id/discount', 'OrderController.applyDiscount')
+  Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
   Route.resource('orders', 'OrderController').apiOnly()
 
   // Image resource routes
   Route.resource('images', 'ImageController').apiOnly()
 
-   // User resource routes
-   Route.resource('users', 'UserController').apiOnly()
+  // User resource routes
+  Route.resource('users', 'UserController').apiOnly()
 })
   .prefix('v1/admin')
   .namespace('Admin')
+  .middleware(['auth', 'is:(admin || manager)'])
